5c3c2210888ba548fdf2aaf1f3c82c09081ae68b
WenTao Xue <89717635+MISTRYUSER@users.noreply.github.com>
2026-09-07 11:27:21 +0800

    test: cover multilingual parser symbols

---
+++ tests/test_multilang_parser.py
@@ -0,0 +1,7 @@
+def test_go_and_typescript_symbols_and_inheritance():
+    from antisentinel.code_map.multilang_parser import MultiLanguageParser
+    p=MultiLanguageParser('multilang-tree-sitter-v1')
+    go=p.parse_file('main.go', b'type Child struct { Base }\nfunc (c Child) Run() {}\n', 's')
+    assert {n.kind for n in go.symbols}=={'class','function'}
+    ts=p.parse_file('main.ts', b'class Child extends Base {}\nfunction run() {}\n', 's')
+    assert {n.kind for n in ts.symbols}=={'class','function'}
